08c5b2bb1844e1a0c601a1bff489b5ccd39b3963
luke-youngmin-cho <83196845+luke-youngmin-cho@users.noreply.github.com>
2021-12-17 22:00:11 +0900

    Operators

---
+++ CSharp_Scripts/UnityLesson_CSharp_Basic/UnityLesson_CSharp_Operator/FunctionsForOperation.cs
@@ -0,0 +1,145 @@
+﻿namespace UnityLesson_CSharp_Operator
+{
+    class FunctionsForOperation
+    {
+        // 산술연산
+        //=================================================================
+        // 덧셈
+        static public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+        static public int Sub(int a, int b)
+        {
+            return a - b;
+        }
+        static public int Div(int a, int b)
+        {
+            return a / b;
+        }
+        static public int Mul(int a, int b)
+        {
+            return a * b;
+        }
+        static public int Mod(int a, int b)
+        {
+            return a % b;
+        }
+
+        // 증감연산
+        static public int Increase(int a)
+        {
+            return a++;
+        }
+        static public int Decrease(int a)
+        {
+            return a--;
+        }
+
+        // 관계연산
+        static public bool IsSame(int a, int b)
+        {
+            return a == b;
+        }
+        static public bool IsDiffrent(int a, int b)
+        {
+            return a != b;
+        }
+        static public bool IsBiffer(int a, int b)
+        {
+            return a > b;
+        }
+        static public bool IsSmaller(int a, int b)
+        {
+            return a < b;
+        }
+        static public bool IsBiggerOrSame(int a, int b)
+        {
+            return a >= b;
+        }
+        static public bool IsSmallerOrSame(int a, int b)
+        {
+            return a <= b;
+        }
+
+        // 대입연산 
+        static public int PlusBToA(int a, int b)
+        {
+            a += b;
+            return a;
+        }
+        static public int MinusBToA(int a, int b)
+        {
+            a -= b;
+            return a;
+        }
+        static public int MultiplyBToA(int a, int b)
+        {
+            a *= b;
+            return a;
+        }
+        static public int DivideBToA(int a, int b)
+        {
+            a /= b;
+            return a;
+        }
+        static public int ModBToA(int a, int b)
+        {
+            a %= b;
+            return a;
+        }
+
+        // 논리연산
+        static public bool LogicOR(bool a, bool b)
+        {
+            return a | b;
+        }
+        static public bool LogicAND(bool a, bool b)
+        {
+            return a & b;
+        }
+        static public bool LogicXOR(bool a, bool b)
+        {
+            return a ^ b;
+        }
+        static public bool LogicNot(bool a)
+        {
+            return !a;
+        }
+        // 조건부 논리연산
+        static public bool ConditionalLogicOR(bool a, bool b)
+        {
+            return a || b;
+        }
+        static public bool ConditionalLogicAND(bool a, bool b)
+        {
+            return a && b;
+        }
+
+        // 비트 연산
+        static public int BitLogicOR(int a, int b)
+        {
+            return a | b;
+        }
+        static public int BitLogicAND(int a, int b)
+        {
+            return a & b;
+        }
+        static public int BitLogicXOR(int a, int b)
+        {
+            return a ^ b;
+        }
+        static public int BitLogicNot(int a)
+        {
+            return ~a;
+        }
+        static public int BitShiftLeft(int a, int howManyBitWantYouToShift)
+        {
+            return a << howManyBitWantYouToShift;
+        }
+        static public int BitShiftRight(int a, int howManyBitWantYouToShift)
+        {
+            return a >> howManyBitWantYouToShift;
+        }
+    }
+}
---
+++ CSharp_Scripts/UnityLesson_CSharp_Basic/UnityLesson_CSharp_Operator/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+
+namespace UnityLesson_CSharp_Operator
+{
+    class Program
+    {
+        static int a = 14;
+        static int b = 5;
+        static int c;
+        
+        static void Main(string[] args)
+        {
+            // 산술 연산  
+            // 더하기, 빼기, 나누기, 곱하기, 나머지
+            //==================================================================
+            // 더하기 
+            c = FunctionsForOperation.Sum(a, b);
+            Console.WriteLine(c);
+            // 빼기
+            c = a - b;
+            Console.WriteLine(c);
+            // 나누기 , 정수를 나누기하면 결과값에서 소수점은 버린다.
+            c = a / b;
+            Console.WriteLine(c);
+            // 곱하기
+            c = a * b;
+            Console.WriteLine(c);
+            // 나머지
+            c = a % b;
+            Console.WriteLine(c);
+
+            // 증감 연산
+            // 증가 연산자, 감소 연산자
+            //==================================================================
+            //증가연산
+            c++; // c = c + 1;
+            Console.WriteLine(c);
+            //감소연산
+            c--; // c = c - 1 ;
+            Console.WriteLine(c);
+
+            // 관계 연산
+            // 같음 , 다름, 크기 비교 연산
+            // 관계 연산의 결과가 참이면 true, 거짓이면 false 를 반환.
+            //==================================================================
+            //같음 비교
+            Console.WriteLine(a == b);
+            //다름 비교
+            Console.WriteLine(a != b);
+            //크다 
+            Console.WriteLine(a > b);
+            //작다
+            Console.WriteLine(a < b);
+            //크거나 같다
+            Console.WriteLine(a >= b);
+            //작거나 같다
+            Console.WriteLine(a <= b);
+
+            // 대입 연산
+            // (더해서, 빼서, 나누어서, 곱해서, 나머지를) 대입
+            //==================================================================
+            c = 20;
+            Console.WriteLine($"Last C value : {c}");
+
+            c += b; // c = c + b;
+            Console.WriteLine(c);
+            c -= b; // c = c - b;
+            Console.WriteLine(c);
+            c /= b; // c = c / b;
+            Console.WriteLine(c);
+            c *= b; // c = c * b;
+            Console.WriteLine(c);
+            c %= b; // c = c % b;
+            Console.WriteLine(c);
+
+            // 논리 연산
+            // 양측의 피연산자들을 비교해서 연산 수행
+            // or , and , xor, not
+            //==================================================================
+            bool A = true;
+            bool B = false;
+            // or 
+            // a 와 b 둘중 하나라도 true 이면 true 반환, 나머지 경우 false 반환
+            Console.WriteLine(A | B);
+
+            // and
+            // a와 b 둘다 true 일때만 true 반환 , 나머지 경우 false 반환
+            Console.WriteLine(A & B);
+            // xor
+            // a 와 b 둘중에 하나만 true 일때 true 반환, 나머지 경우 false 반환
+            Console.WriteLine(A ^ B);
+            // not 
+            // a 의 반대 부호 반환 ( true 이면 false, false 이면 true 반환)
+            Console.WriteLine(!A);
+
+            // 조건부 논리연산
+            // 왼쪽 피연산자 조건에 따라서 오른쪽 피연산자와 비교할지 말지 평가 후에 연산한다.
+            //=================================================================================
+            // or 조건부 논리연산
+            // 만약 A 가 true 이면 B 의 값에 관계없이 결과값이 true 이므로 
+            // 연산을 하지 않고 A 값을 반환함.
+            Console.WriteLine(A || B);
+            // and 조건부 논리연산
+            //만약 A 가 false 이면 B 의 값에 관계없이 결과값이 false 이므로
+            // 연산을 하지 않고 A 값을 반환함.
+            Console.WriteLine(A && B);
+
+            // 비트 연산
+            // 비트연산 하는 이유 : 데이터를 효율적으로 관리하기 위함 
+            // or, and, xor , not shift-left, shift-right
+            int howManyBitYouWantToShift = 1;
+            // or
+            Console.WriteLine(a | b);
+            // and
+            Console.WriteLine(a & b);
+            // xor
+            Console.WriteLine(a ^ b);
+            // not
+            Console.WriteLine(~a);
+            // shift- left
+            Console.WriteLine(a << howManyBitYouWantToShift);
+            // shift- right
+            Console.WriteLine(a >> howManyBitYouWantToShift);
+
+        }
+    }
+
+    // FSM Finite State Machine 유한 상태머신 ( 상태의 개수가 정해진 머신 )
+    // 클래스의 상태에 따라 다른동작을 하기위해 사용. 
+
+    // ex) 플레이어가 Idle 상태면 마을로 귀환이 가능하고, 피격당한상태와 공격중인 상태일때 // 귀환이 불가능하게 설정하고 싶으면  
+    // 플레이어의 FSM 을 만들어서 상태를 나누고 귀환이 가능한 조건을 FSM 으로 구분한다.  
+    // 즉 사용자가 귀환 버튼을 눌렀을때 , IDLE 상태면 귀환 모션이 실행되면서 귀환을 시전하지만 
+    //, 몬스터에게 피격당하거나 공격중인 상태일때 귀환 버튼을 누르면  FSM 조건에 따라 귀환이 
+    //불가능하게 한다.  
+    [Flags]
+    public enum e_FSM 
+    {
+        IDLE = 0,
+        피격당한상태 = 1 << 0,
+        공격중인상태 = 1 << 1,
+        STATE_3 = 1 << 2,
+        STATE_4 = 1 << 3,
+    }
+}
