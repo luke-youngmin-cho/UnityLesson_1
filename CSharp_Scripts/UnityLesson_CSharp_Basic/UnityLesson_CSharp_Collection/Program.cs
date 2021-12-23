ffb7b3b5dd94ec7864bba37b5a11f0aca75e5066
LukeYoungminCho <83196845+luke-youngmin-cho@users.noreply.github.com>
2021-12-23 23:51:21 +0900

    Added comments in Collection projects
    additional explanations

--- CSharp_Scripts/UnityLesson_CSharp_Basic/UnityLesson_CSharp_Collection/Program.cs
+++ CSharp_Scripts/UnityLesson_CSharp_Basic/UnityLesson_CSharp_Collection/Program.cs
@@ -1,5 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+
+// Collection 은 동적배열이라고도 함. 
+// Array 와 다르게 크기를 정해놓지않고 
+// Collection 단어 뜻대로 수집하듯이 
+// 아이템들을 추가하거나 뺄 수 있으며, 그에따라 데이터 크기가 유동적으로 변한다.
 namespace UnityLesson_CSharp_Collection
 {
     class Program
@@ -26,6 +31,7 @@ namespace UnityLesson_CSharp_Collection
                 Console.WriteLine(tmpNumber);
             }
 
+            // foreach 는 collection 이 가지고있는 타입의 아이템만큼 반복문을 실행하면서 각 아이템을 반환해준다.
             foreach (int num in _list)
             {
                 Console.WriteLine(num);
@@ -50,26 +56,47 @@ namespace UnityLesson_CSharp_Collection
                 Console.WriteLine("검사를 찾을 수 없습니다");
             }
 
+
+            // dictionary 도 foreach 구문이 가능하다.
+            // dictionary 도 collection 이고
+            // dictionary 의 key , value 도 각각 collection 이다. 
+            // 즉 , dictionary 자체도 foreach 문이 가능하고, key, value 도 각각 foreach 구문이 가능하다. 
+            // 아래 세가지 예시 :
+
+            // dictionary 의 key 값 을 foreach 문을 통해 반복문 실행
             foreach (string sub in _dic.Keys)
             {   
                 string tmpValue = _dic[sub];
                 Console.WriteLine($"{sub} : {tmpValue}");
             }
-
+            // dictionary 의 value 값 을 foreach 문을 통해 반복문 실행
             foreach (string sub in _dic.Values)
             {
                 Console.WriteLine(sub);
             }
-
+            // dictionary를 foreach 문을 통해 반복문 실행
             foreach (KeyValuePair<string, string> sub in _dic)
             {
                 string tmpKey = sub.Key;
                 string tmpValue = sub.Value;
                 Console.WriteLine($"{tmpKey} : {tmpValue}");
             }
+
+            // 위 세가지 경우가 있지만, 
+            // dictionary 는 단어 뜻대로 사전 처럼 사용하기 위한 collection 이다.
+            // 우리가 사전을 찾을때 , 
+            // 단어를 먼저 찾고 거기에 따라 단어의 뜻을 찾는다. 
+            // dictionary 에서 key 는 단어, value 는 단어의 뜻과 같은 역할이다.
+            // 따라서 foreach 문을 사용해서 접근할때도,
+            // dictionary 의 key 로 반복문을 통해 접근하는 경우가 대부분이지, 
+            // value 나 dictionary 자체로 접근하는 경우는 잘 없다. 
+            // 마치 '사과' 라는 단어를 검색하면 '사과나무에서 열리는 열매' 라는 뜻을 찾는것이지
+            // '사과나무에서 열리는 열매' 를 검색해서 '사과'를 찾거나
+            // '사과 : 사과나무에서 열리는 열매' 자체를 검색하지 않는것과 같다고 생각하면 된다. 
+           
             
 
-            
+            // 다음시간에...
             //--------------------------------
             // Stack
             //--------------------------------
