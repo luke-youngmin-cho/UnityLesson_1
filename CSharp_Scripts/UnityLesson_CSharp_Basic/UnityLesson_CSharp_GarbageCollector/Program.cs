deede86f615c9fbd17eaab55a185b458de51be51
luke-youngmin-cho <83196845+luke-youngmin-cho@users.noreply.github.com>
2021-12-30 22:02:23 +0900

    2021_12_30
    ..

--- CSharp_Scripts/UnityLesson_CSharp_Basic/UnityLesson_CSharp_GarbageCollector/Program.cs
+++ CSharp_Scripts/UnityLesson_CSharp_Basic/UnityLesson_CSharp_GarbageCollector/Program.cs
@@ -6,7 +6,23 @@ namespace UnityLesson_CSharp_GarbageCollector
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            AA aa = new AA();
         }
+    }
+
+    class AA
+    {
+        int age;
+        public AA() // 생성자
+        {
+        }
+
+        ~AA() // 소멸자
+        {
+        }
+
+        // C# 에서 소멸자를 따로 호출하지 않는 이유
+        // GC (Garbage Collector) 가 런타임으로 쓰레기 객체를 검색해서
+        // 삭제하기 때문에 생성한 객체를 따로 메모리에서 지워주지 않아도 된다.
     }
 }
